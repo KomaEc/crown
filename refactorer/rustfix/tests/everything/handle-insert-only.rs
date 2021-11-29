bee652510b645efe5bb3a2e19d24fa3f21dcd0b5
zhl <hanliangzhang.cm@gmail.com>
2021-11-29 13:45:25 +0000

    Rm bc; rm old rustfix

--- refactorer/rustfix/tests/everything/handle-insert-only.rs
+++
@@ -1,8 +0,0 @@
-fn main() {
-    // insert only fix, adds `,` to first match arm
-    // why doesnt this replace 1 with 1,?
-    match &Some(3) {
-        &None => 1
-        &Some(x) => x,
-    };
-}
--- refactorer/rustfix/tests/everything/lt-generic-comp.rs
+++
@@ -1,7 +0,0 @@
-fn main() {
-    let x = 5i64;
-
-    if x as u32 < 4 {
-        println!("yay");
-    }
-}
