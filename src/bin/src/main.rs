b92ef749ab397956351c166cc94466f52f19fc4f
zhl <hanliangzhang.cm@gmail.com>
2022-01-17 12:52:27 +0000

    Minor

--- src/bin/src/main.rs
+++ src/bin/src/main.rs
@@ -3,7 +3,7 @@
 
 extern crate rustc_interface;
 
-use crustr_rustc_interface;
+use crustr_rustc_interface::{config_setup, toy_run};
 use std::env;
 
 fn main() {
@@ -16,6 +16,6 @@ fn main() {
     let input_file_name: &str = &args[1];
     // crustr_rustc_interface::toy_run::run(input_file_name.to_string());
 
-    let config = crustr_rustc_interface::config_setup(input_file_name.into());
-    rustc_interface::run_compiler(config, crustr_rustc_interface::toy_run::run)
+    let config = config_setup(input_file_name.into());
+    rustc_interface::run_compiler(config, toy_run::run)
 }
