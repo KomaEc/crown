446021bb8a5fb13cb3c71f571d2004144271bede
zhl <hanliangzhang.cm@gmail.com>
2022-03-06 10:19:45 +0000

    Add manually translated buffer

---
+++ manually_translated/buffer/lib.rs
@@ -0,0 +1,23 @@
+#![allow(dead_code)]
+#![allow(mutable_transmutes)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+#![allow(non_upper_case_globals)]
+#![allow(unused_assignments)]
+#![allow(unused_mut)]
+#![feature(register_tool)]
+#![register_tool(c2rust)]
+#![feature(ptr_internals)]
+#![feature(new_uninit)]
+#![feature(maybe_uninit_slice)]
+#![feature(slice_ptr_get)]
+
+extern crate libc;
+
+pub mod src {
+    pub mod buffer;
+    // pub mod buffer_safe;
+    // pub mod buffer_safer;
+    pub mod buffer_slice;
+    // pub mod test;
+} // mod src
