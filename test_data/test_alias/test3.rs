4de4204fa2aa9195550f53404c08f794f685efbb
zhl <hanliangzhang.cm@gmail.com>
2022-07-27 19:45:32 +0100

    clean up

--- test_data/test_alias/test3.rs
+++
@@ -1,4 +0,0 @@
-fn f(mut p: *mut i32, q: *mut i32) {
-    *p = *q;
-    p = &mut *q;
-}
--- test_data/test_structs/test1.rs
+++
@@ -1,8 +0,0 @@
-pub struct X {
-    f: Option<std::ptr::NonNull<i32>>,
-    g: *mut i32
-}
-
-pub struct Y {
-    h: Option<std::ptr::NonNull<Option<std::ptr::NonNull<i32>>>>,
-}
