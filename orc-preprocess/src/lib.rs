f3b3210b4ff76920f72bf38bfc6b50c41b5e86b1
zhl <hanliangzhang.cm@gmail.com>
2022-08-24 18:38:32 +0100

    implement null stmt insertor as an hir visitor

--- orc-preprocess/src/lib.rs
+++ orc-preprocess/src/lib.rs
@@ -7,7 +7,10 @@ extern crate rustc_middle;
 extern crate rustc_span;
 
 use orc_common::rewriter::{RewriteMode, Rewriter};
-use rustc_hir::{Block, Expr, ExprKind, ItemKind, OwnerNode, StmtKind, UnOp};
+use rustc_hir::{
+    intravisit::{self, Visitor},
+    Expr, ExprKind, ItemKind, OwnerNode, UnOp,
+};
 use rustc_middle::ty::TyCtxt;
 use rustc_span::BytePos;
 
@@ -15,64 +18,41 @@ pub fn preprocess(tcx: TyCtxt, mode: RewriteMode) {
     insert_null_statement(tcx, mode)
 }
 
-fn insert_null_stmt_to_branch(
-    tcx: TyCtxt,
-    stmt_str: String,
-    branch: &Expr,
-    rewriter: &mut Rewriter,
-) {
-    let branch_span_lo = branch.span.lo();
-    let empty_span_after_curly_brace = branch
-        .span
-        .with_lo(branch_span_lo + BytePos(1))
-        .shrink_to_lo();
-    rewriter.make_suggestion(tcx, empty_span_after_curly_brace, String::new(), stmt_str)
+struct NullStmtInsertor<'me, 'hir> {
+    tcx: TyCtxt<'hir>,
+    rewriter: &'me mut Rewriter,
 }
-
-fn insert_null_statement_for_block(tcx: TyCtxt, block: &Block, rewriter: &mut Rewriter) {
-    block
-        .stmts
-        .iter()
-        .filter_map(|stmt| match stmt.kind {
-            StmtKind::Expr(expr) | StmtKind::Semi(expr) => Some(expr),
-            _ => None,
-        })
-        .chain(block.expr)
-        .for_each(|expr| match expr.kind {
-            ExprKind::Block(inner_block, _) => {
-                insert_null_statement_for_block(tcx, inner_block, rewriter)
+impl<'me, 'hir> Visitor<'hir> for NullStmtInsertor<'me, 'hir> {
+    fn visit_expr(&mut self, expr: &'hir Expr<'hir>) {
+        if let ExprKind::If(cond, truth_branch, false_branch) = expr.kind {
+            let ExprKind::DropTemps(mut cond) = cond.kind else {
+                panic!("for some reasons, if conditions are all droptemps.");
+            };
+            // peel off negations
+            let mut sign = true;
+            while let ExprKind::Unary(UnOp::Not, expr) = cond.kind {
+                sign = !sign;
+                cond = expr;
             }
-            ExprKind::If(cond, truth_branch, false_branch) => {
-                let ExprKind::DropTemps(mut cond) = cond.kind else {
-                    panic!("for some reasons, if conditions are all droptemps.");
-                };
 
-                // peel off negations
-                let mut sign = true;
-                while let ExprKind::Unary(UnOp::Not, expr) = cond.kind {
-                    sign = !sign;
-                    cond = expr;
-                }
+            match cond.kind {
+                ExprKind::MethodCall(path, args, _) if path.ident.as_str() == "is_null" => {
+                    let ptr = args.first().unwrap();
 
-                match cond.kind {
-                    ExprKind::MethodCall(path, args, _) if path.ident.as_str() == "is_null" => {
-                        let ptr = args.first().unwrap();
-
-                        // currently we only rewrite variables not complex expressions
-                        let ExprKind::Path(..) = ptr.kind else { return };
-
-                        let stmt_str = rustc_hir_pretty::id_to_string(&tcx.hir(), ptr.hir_id)
+                    // currently we only rewrite variables not complex expressions
+                    if let ExprKind::Path(..) = ptr.kind {
+                        let stmt_str = rustc_hir_pretty::id_to_string(&self.tcx.hir(), ptr.hir_id)
                             + " = 0 as *mut _;";
 
                         if sign {
-                            insert_null_stmt_to_branch(tcx, stmt_str, truth_branch, rewriter)
+                            self.insert_to_branch(stmt_str, truth_branch);
                         } else {
                             if let Some(false_branch) = false_branch {
-                                insert_null_stmt_to_branch(tcx, stmt_str, false_branch, rewriter)
+                                self.insert_to_branch(stmt_str, false_branch);
                             } else {
                                 let empty_span_after_curly_brace = truth_branch.span.shrink_to_hi();
-                                rewriter.make_suggestion(
-                                    tcx,
+                                self.rewriter.make_suggestion(
+                                    self.tcx,
                                     empty_span_after_curly_brace,
                                     String::new(),
                                     "else { ".to_string() + &stmt_str + " }",
@@ -80,13 +60,31 @@ fn insert_null_statement_for_block(tcx: TyCtxt, block: &Block, rewriter: &mut Re
                             }
                         }
 
-                        // println!("The span of truth branch: {:?}", truth_branch.span);
+                        return;
                     }
-                    _ => {}
                 }
+                _ => {}
             }
-            _ => {}
-        });
+        }
+
+        intravisit::walk_expr(self, expr)
+    }
+}
+
+impl<'me, 'hir> NullStmtInsertor<'me, 'hir> {
+    fn insert_to_branch(&mut self, stmt_str: String, branch: &Expr) {
+        let branch_span_lo = branch.span.lo();
+        let empty_span_after_curly_brace = branch
+            .span
+            .with_lo(branch_span_lo + BytePos(1))
+            .shrink_to_lo();
+        self.rewriter.make_suggestion(
+            self.tcx,
+            empty_span_after_curly_brace,
+            String::new(),
+            stmt_str,
+        )
+    }
 }
 
 fn insert_null_statement(tcx: TyCtxt, mode: RewriteMode) {
@@ -97,8 +95,11 @@ fn insert_null_statement(tcx: TyCtxt, mode: RewriteMode) {
         let OwnerNode::Item(item) = owner.node() else { continue };
         let ItemKind::Fn(_, _, body_id) = item.kind else { continue };
         let hir_body = tcx.hir().body(body_id);
-        let ExprKind::Block(block, _) = hir_body.value.kind else { todo!("function body is assumed to be a block") };
-        insert_null_statement_for_block(tcx, block, &mut rewriter)
+        NullStmtInsertor {
+            rewriter: &mut rewriter,
+            tcx,
+        }
+        .visit_expr(&hir_body.value);
     }
 
     rewriter.write(mode)
