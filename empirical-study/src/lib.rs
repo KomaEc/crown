7e2c1cae107ba5248a1a4c81a12b13ae4ab1becd
zhl <hanliangzhang.cm@gmail.com>
2022-09-15 10:33:23 +0100

    minor

--- empirical-study/src/lib.rs
+++ empirical-study/src/lib.rs
@@ -22,7 +22,7 @@ extern crate rustc_type_ir;
 
 use orc_common::OrcInput;
 use orc_taint_analysis::taint_results;
-use rustc_middle::mir::{visit::Visitor, Location, Rvalue};
+// use rustc_middle::mir::{visit::Visitor, Location, Rvalue};
 
 use cli_table::{format::Justify, print_stdout, Cell, Style, Table};
 
@@ -40,56 +40,56 @@ pub trait EmpiricalStudy<'tcx> {
         }
 
         perform![
-            compute_percentage_of_non_address_taking_functions,
+            // compute_percentage_of_non_address_taking_functions,
             compute_percentage_of_non_self_referential_structs
         ];
     }
 
-    fn compute_percentage_of_non_address_taking_functions(&self);
+    // fn compute_percentage_of_non_address_taking_functions(&self);
     fn compute_percentage_of_non_self_referential_structs(&self);
 }
 
 impl<'tcx, Input: OrcInput<'tcx>> EmpiricalStudy<'tcx> for Input {
-    fn compute_percentage_of_non_address_taking_functions(&self) {
-        struct Vis;
-        impl<'tcx> Visitor<'tcx> for Vis {
-            fn visit_rvalue(&mut self, rvalue: &Rvalue<'tcx>, _location: Location) {
-                if let Rvalue::AddressOf(rustc_ast::Mutability::Mut, _) = rvalue {
-                    panic!("{:?} to be catched", rvalue)
-                }
-            }
-        }
-        let prev_hook = std::panic::take_hook();
-        std::panic::set_hook(Box::new(|_| {}));
-        let n_address_taking_functions = self
-            .functions()
-            .iter()
-            .filter(|&did| {
-                let body = self.tcx().optimized_mir(did);
-                std::panic::catch_unwind(std::panic::AssertUnwindSafe(|| Vis.visit_body(body)))
-                    .is_ok()
-            })
-            .count();
-        std::panic::set_hook(prev_hook);
-        let percentage = format!(
-            "{:.1}%",
-            100.0 * n_address_taking_functions as f64 / self.functions().len() as f64
-        );
-        let table = vec![vec![
-            self.functions().len().cell().justify(Justify::Right),
-            n_address_taking_functions.cell().justify(Justify::Right),
-            percentage.cell().justify(Justify::Right),
-        ]]
-        .table()
-        .title(vec![
-            "# Functions".cell().bold(true),
-            "# Functions without &mut".cell().bold(true),
-            "Percentage".cell().bold(true),
-        ])
-        .bold(true);
+    // fn compute_percentage_of_non_address_taking_functions(&self) {
+    //     struct Vis;
+    //     impl<'tcx> Visitor<'tcx> for Vis {
+    //         fn visit_rvalue(&mut self, rvalue: &Rvalue<'tcx>, _location: Location) {
+    //             if let Rvalue::AddressOf(rustc_ast::Mutability::Mut, _) = rvalue {
+    //                 panic!("{:?} to be catched", rvalue)
+    //             }
+    //         }
+    //     }
+    //     let prev_hook = std::panic::take_hook();
+    //     std::panic::set_hook(Box::new(|_| {}));
+    //     let n_address_taking_functions = self
+    //         .functions()
+    //         .iter()
+    //         .filter(|&did| {
+    //             let body = self.tcx().optimized_mir(did);
+    //             std::panic::catch_unwind(std::panic::AssertUnwindSafe(|| Vis.visit_body(body)))
+    //                 .is_ok()
+    //         })
+    //         .count();
+    //     std::panic::set_hook(prev_hook);
+    //     let percentage = format!(
+    //         "{:.1}%",
+    //         100.0 * n_address_taking_functions as f64 / self.functions().len() as f64
+    //     );
+    //     let table = vec![vec![
+    //         self.functions().len().cell().justify(Justify::Right),
+    //         n_address_taking_functions.cell().justify(Justify::Right),
+    //         percentage.cell().justify(Justify::Right),
+    //     ]]
+    //     .table()
+    //     .title(vec![
+    //         "# Functions".cell().bold(true),
+    //         "# Functions without &mut".cell().bold(true),
+    //         "Percentage".cell().bold(true),
+    //     ])
+    //     .bold(true);
 
-        assert!(print_stdout(table).is_ok());
-    }
+    //     assert!(print_stdout(table).is_ok());
+    // }
 
     fn compute_percentage_of_non_self_referential_structs(&self) {
         let taint_results = taint_results(self);
