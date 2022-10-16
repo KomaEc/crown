719682d3c3db4e18d07adcbaa8bb5255951fe3a2
zhl <hanliangzhang.cm@gmail.com>
2022-10-16 12:42:30 +0100

    refactor

--- crates/refactor/src/lib.rs
+++ crates/refactor/src/lib.rs
@@ -4,6 +4,8 @@
 #![feature(split_array)]
 #![feature(let_else)]
 
+// use analysis::{ownership::{OwnershipSchemes, self}, type_qualifier::flow_insensitive::{mutability::{MutabilityResult, self}, fatness::{self, FatnessResult}}};
+
 extern crate rustc_ast;
 extern crate rustc_ast_pretty;
 extern crate rustc_error_codes;
@@ -20,3 +22,18 @@ extern crate rustc_passes;
 extern crate rustc_session;
 extern crate rustc_span;
 extern crate rustc_target;
+
+
+// pub struct PointerQualifier {
+//     mutability: mutability::Mutability,
+//     fatness: fatness::Fatness,
+//     ownership: ownership::Ownership,
+//     nullability: ()
+// }
+
+// pub struct RefactorCtxt<O: for<'analysis> OwnershipSchemes<'analysis>> {
+//     ownership_schemes: O,
+//     mutability_result: MutabilityResult,
+//     fatness_result: FatnessResult,
+//     nullability_result: (),
+// }
