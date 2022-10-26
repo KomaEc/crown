f4df51cf580b725c7b31e3e40058e9f3cec14734
zhl <hanliangzhang.cm@gmail.com>
2022-10-26 15:09:58 +0100

    fmt

--- crates/analysis/src/ssa/constraint/infer.rs
+++ crates/analysis/src/ssa/constraint/infer.rs
@@ -536,7 +536,7 @@ impl<'rn, 'tcx: 'rn> Renamer<'rn, 'tcx> {
                 // dest of deref_copy is never interpretted
                 let _ = self.state.try_consume_at(lhs.local, location);
                 // let lhs_consume =
-                    // consume_place_at::<Infer>(lhs, self.body, location, self, infer_cx);
+                // consume_place_at::<Infer>(lhs, self.body, location, self, infer_cx);
                 // assert!(lhs_consume.is_none(), "{:?}", lhs_consume.unwrap());
                 let rhs_consume =
                     consume_place_at::<Infer>(rhs, self.body, location, self, infer_cx);
