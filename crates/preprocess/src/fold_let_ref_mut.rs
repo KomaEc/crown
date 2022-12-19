1531516a68b293ab3aa99372743da9694af651c2
zhl <hanliangzhang.cm@gmail.com>
2022-12-19 15:04:37 +0000

    handle more patterns of let ref mut

--- crates/preprocess/src/fold_let_ref_mut.rs
+++ crates/preprocess/src/fold_let_ref_mut.rs
@@ -19,16 +19,24 @@ pub fn fold_let_ref_mut(tcx: TyCtxt, mode: RewriteMode) {
         })
         .collect::<HashSet<_>>();
 
-    let re = concat!(
+    let pattern1 = concat!(
         r"let ref mut fresh(?P<version1>[0-9]+)[\s|\n]*=[\s|\n]*(?P<lhs>[^;]+);",
         r"[\s|\n]*\*fresh(?P<version2>[0-9]+)[\s|\n]*=[\s|\n]*(?P<rhs>[^;]+);"
     );
 
-    let re = regex::Regex::new(re).unwrap();
+    let re1 = regex::Regex::new(pattern1).unwrap();
+
+    let pattern2 = concat!(
+        r"let ref mut fresh(?P<version1>[0-9]+)[\s|\n]*=[\s|\n]*(?P<lhs>[^;]+);[\s|\n]*",
+        r"let fresh(?P<version2>[0-9]+)[\s|\n]*=[\s|\n]*\*fresh(?P<version3>[0-9]+)[^;]*;[\s|\n]*",
+        r"(?P<incr>\*fresh(?P<version4>[0-9]+)[\s|\n]*=[\s|\n]*\(\*fresh(?P<version5>[0-9]+)\)\.wrapping_add\(1\)[\s|\n]*;)"
+    );
+
+    let re2 = regex::Regex::new(pattern2).unwrap();
 
     for file_name in user_files.iter() {
         let source = fs::read_to_string(file_name).unwrap();
-        let replacement = re
+        let replacement = re1
             .replace_all(&source, |caps: &regex::Captures| {
                 let original = &caps[0];
                 let version1 = &caps["version1"];
@@ -48,6 +56,36 @@ pub fn fold_let_ref_mut(tcx: TyCtxt, mode: RewriteMode) {
                     .replace_all(rhs, lhs);
 
                 lhs.to_owned() + " = " + &rhs + ";"
+            })
+            .to_string();
+
+        let replacement = re2
+            .replace_all(&replacement, |caps: &regex::Captures| {
+                let original = &caps[0];
+                let version1 = &caps["version1"];
+                let version2 = &caps["version2"];
+                let version3 = &caps["version3"];
+                let version4 = &caps["version4"];
+                let version5 = &caps["version5"];
+
+                if !(version1 == version3
+                    && version1 == version4
+                    && version1 == version5
+                    && version1 != version2)
+                {
+                    return original.to_owned();
+                }
+
+                let expr_fresh = r"\*fresh".to_owned() + version1;
+
+                let lhs = &caps["lhs"];
+                let incr = &caps["incr"];
+
+                let incr = regex::Regex::new(&expr_fresh)
+                    .unwrap()
+                    .replace_all(incr, lhs);
+
+                r"let fresh".to_owned() + version2 + " = " + lhs + ";" + &incr
             })
             .to_string();
 
