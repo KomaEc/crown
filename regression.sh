814c12d7724aa8119b9dbfaf6f26d2e0c91047d9
zhl <hanliangzhang.cm@gmail.com>
2023-01-08 21:36:42 +0000

    update scripts

--- regression.sh
+++ regression.sh
@@ -1,4 +1,18 @@
 
+RUSTC_PATH=$(rustc --print sysroot)/lib
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    # add rustc lib to dyld path
+    if [ -d "$RUSTC_PATH" ] && [[ ":$DYLD_LIBRARY_PATH:" != *":$RUSTC_PATH:"* ]]; then
+        export DYLD_LIBRARY_PATH="${DYLD_LIBRARY_PATH:+"$DYLD_LIBRARY_PATH:"}$RUSTC_PATH"
+    fi
+fi
+
+echo "start building crown.."
+cargo build --release
+
+CROWN=target/release/crown
+
 FILE_CNT=0
 
 if [ $# -eq 0 ]; then
@@ -11,6 +25,6 @@ echo "writing results into $regressions"
 
 
 for f in $(cat regression-invocations) ; do
-    cargo run -r -- $f analyse > "$regressions/$FILE_CNT"
+    $CROWN $f analyse > "$regressions/$FILE_CNT"
     let FILE_CNT=FILE_CNT+1
 done
