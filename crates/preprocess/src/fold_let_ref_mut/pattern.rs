64634874d4debcb9602af36d9c3ab91df6772248
zhl <hanliangzhang.cm@gmail.com>
2023-01-10 22:48:19 +0000

    update patterns

--- crates/preprocess/src/fold_let_ref_mut/pattern.rs
+++ crates/preprocess/src/fold_let_ref_mut/pattern.rs
@@ -1,4 +1,6 @@
 pub const PATTERNS: &[Pattern] = &[
+    ASSIGN_VALUE_AS_ASSIGNER4,
+    ASSIGN_VALUE_AS_ASSIGNER3,
     ASSIGN_VALUE_AS_ASSIGNER1,
     ASSIGN_VALUE_AS_ASSIGNER2,
     ASSIGN_VALUE_IN_CONDITION,
@@ -9,6 +11,120 @@ pub const PATTERNS: &[Pattern] = &[
 pub struct Pattern {
     pub pattern: &'static str,
     pub replacer: for<'t> fn(&regex::Captures<'t>) -> String,
+}
+
+
+const ASSIGN_VALUE_AS_ASSIGNER4: Pattern = Pattern {
+    pattern: concat!(
+        r"let ref mut fresh(?P<version1>[0-9]+)[\s|\n]*=[\s|\n]*(?P<w>[^;]+);[\s|\n]*",
+        r"\*fresh(?P<version2>[0-9]+)[\s|\n]*=[\s|\n]*(?P<val>[^;]+);[\s|\n]*",
+        r"let ref mut fresh(?P<version3>[0-9]+)[\s|\n]*=[\s|\n]*(?P<z>[^;]+);[\s|\n]*",
+        r"\*fresh(?P<version4>[0-9]+)[\s|\n]*=[\s|\n]*\*fresh(?P<version5>[0-9]+);[\s|\n]*",
+        r"let ref mut fresh(?P<version6>[0-9]+)[\s|\n]*=[\s|\n]*(?P<y>[^;]+);[\s|\n]*",
+        r"\*fresh(?P<version7>[0-9]+)[\s|\n]*=[\s|\n]*\*fresh(?P<version8>[0-9]+);[\s|\n]*",
+        r"let ref mut fresh(?P<version9>[0-9]+)[\s|\n]*=[\s|\n]*(?P<x>[^;]+);[\s|\n]*",
+        r"\*fresh(?P<version10>[0-9]+)[\s|\n]*=[\s|\n]*\*fresh(?P<version11>[0-9]+);",
+    ),
+    replacer: assign_value_as_assigner4,
+};
+
+fn assign_value_as_assigner4(caps: &regex::Captures<'_>) -> String {
+
+    let original = &caps[0];
+    let version1 = &caps["version1"];
+    let version2 = &caps["version2"];
+    let version3 = &caps["version3"];
+    let version4 = &caps["version4"];
+    let version5 = &caps["version5"];
+    let version6 = &caps["version6"];
+    let version7 = &caps["version7"];
+    let version8 = &caps["version8"];
+    let version9 = &caps["version9"];
+    let version10 = &caps["version10"];
+    let version11 = &caps["version11"];
+
+
+    if !(version1 == version2
+        && version1 != version3
+        && version3 == version4
+        && version1 == version5
+        && version1 != version6
+        && version3 != version6
+        && version6 == version7
+        && version3 == version8
+        && version1 != version9
+        && version3 != version9
+        && version6 != version9
+        && version9 == version10
+        && version6 == version11)
+    {
+        return original.to_owned();
+    }
+
+    let x = &caps["x"];
+    let y = &caps["y"];
+    let z = &caps["z"];
+    let w = &caps["w"];
+    let val = &caps["val"];
+
+    w.to_owned() + " = " + val + "; " + z + " = " + w + "; " + y + " = " + z + "; " + x + " = " + y + ";"
+}
+
+/// ```c
+/// x = y = z = value;
+/// ```
+/// is translated to
+/// ```rust
+/// let ref mut fresh1 = z;
+/// *fresh1 = val;
+/// let ref mut fresh2 = y;
+/// *fresh2 = *fresh1;
+/// let ref mut fresh3 = x;
+/// *fresh3 = *fresh2;
+/// ```
+const ASSIGN_VALUE_AS_ASSIGNER3: Pattern = Pattern {
+    pattern: concat!(
+        r"let ref mut fresh(?P<version1>[0-9]+)[\s|\n]*=[\s|\n]*(?P<z>[^;]+);[\s|\n]*",
+        r"\*fresh(?P<version2>[0-9]+)[\s|\n]*=[\s|\n]*(?P<val>[^;]+);[\s|\n]*",
+        r"let ref mut fresh(?P<version3>[0-9]+)[\s|\n]*=[\s|\n]*(?P<y>[^;]+);[\s|\n]*",
+        r"\*fresh(?P<version4>[0-9]+)[\s|\n]*=[\s|\n]*\*fresh(?P<version5>[0-9]+);[\s|\n]*",
+        r"let ref mut fresh(?P<version6>[0-9]+)[\s|\n]*=[\s|\n]*(?P<x>[^;]+);[\s|\n]*",
+        r"\*fresh(?P<version7>[0-9]+)[\s|\n]*=[\s|\n]*\*fresh(?P<version8>[0-9]+);",
+    ),
+    replacer: assign_value_as_assigner3,
+};
+
+fn assign_value_as_assigner3(caps: &regex::Captures<'_>) -> String {
+
+    let original = &caps[0];
+    let version1 = &caps["version1"];
+    let version2 = &caps["version2"];
+    let version3 = &caps["version3"];
+    let version4 = &caps["version4"];
+    let version5 = &caps["version5"];
+    let version6 = &caps["version6"];
+    let version7 = &caps["version7"];
+    let version8 = &caps["version8"];
+
+
+    if !(version1 == version2
+        && version1 != version3
+        && version3 == version4
+        && version1 == version5
+        && version1 != version6
+        && version3 != version6
+        && version6 == version7
+        && version3 == version8)
+    {
+        return original.to_owned();
+    }
+
+    let x = &caps["x"];
+    let y = &caps["y"];
+    let z = &caps["z"];
+    let val = &caps["val"];
+
+    z.to_owned() + " = " + val + "; " + y + " = " + z + "; " + x + " = " + y + ";"
 }
 
 /// ```c
@@ -38,6 +154,7 @@ fn assign_value_as_assigner1(caps: &regex::Captures<'_>) -> String {
     let version3 = &caps["version3"];
     let version4 = &caps["version4"];
     let version5 = &caps["version5"];
+
 
     if !(version1 == version2
         && version1 != version3
