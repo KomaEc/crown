5efe4e4dde1d89ca81a11001d914d08b0eb7b151
zhl <hanliangzhang.cm@gmail.com>
2023-01-11 11:43:53 +0000

    bug fixed in libc_call

--- crates/refactor/src/rewrite_fn/libc_call.rs
+++ crates/refactor/src/rewrite_fn/libc_call.rs
@@ -75,29 +75,16 @@ impl<'tcx, 'me> FnRewriteCtxt<'tcx, 'me> {
         location: Location,
         rewriter: &mut impl Rewrite,
     ) {
-        let FnRewriteCtxt {
-            body,
-            def_use_chain,
-            ..
-        } = *self;
-
         for arg in args.iter().skip(1) {
             if let Some(place) = arg.place() {
                 let Some(local) = place.as_local() else { panic!() };
-                let def_loc = def_use_chain.def_loc(local, location);
-                let RichLocation::Mir(def_loc) = def_loc else { panic!() };
-                let Left(stmt) = body.stmt_at(def_loc) else {
-                    // TODO correctness?
-                    return
-                };
-                let StatementKind::Assign(box (_, rvalue)) = &stmt.kind else { panic!() };
                 let ty = self.body.local_decls[local].ty;
                 let required = if ty.is_unsafe_ptr() {
                     PlaceValueType::Ptr(&[PointerKind::Raw(RawMeta::Const)])
                 } else {
                     PlaceValueType::Irrelavent
                 };
-                self.rewrite_rvalue_at(rvalue, def_loc, stmt.source_info.span, required, rewriter);
+                self.rewrite_temporary(local, location, required, rewriter);
             }
         }
     }
