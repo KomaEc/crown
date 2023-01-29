16056ff22e1f785e70f3b8d8759cd2aaaee247b6
zhl <hanliangzhang.cm@gmail.com>
2023-01-29 21:04:11 +0000

    count number of structs

--- benchmark_statistics.sh
+++ benchmark_statistics.sh
@@ -5,12 +5,13 @@ PROJ_DIR=$(dirname $0)
 
 BENCHMARK=$PROJ_DIR/benchmark
 
-echo "Benchmark Name,#Files,#Functions,LOC"
+echo "Benchmark Name,#Files,#Structs,#Functions,LOC"
 for f in $(ls $BENCHMARK); do
     NLOC=$(find $BENCHMARK/$f -type f -name "*.rs" | xargs wc -l | rg 'total' | tr -d -c 0-9)
     NFNS=$(find $BENCHMARK/$f -type f -name "*.rs" | xargs rg 'unsafe[ |\n]*(extern "C")?[ |\n]*fn[ |\n]*[^\(]' | wc -l | tr -d -c 0-9)
+    NSTRUCTS=$(find $BENCHMARK/$f -type f -name "*.rs" | xargs rg 'struct[ |\n]*' | wc -l | tr -d -c 0-9)
     NFILES=$(find $BENCHMARK/$f -type f -name "*.rs" | wc -l | tr -d -c 0-9)
     # exclude lib.rs and build.rs
     NFILES=$(echo "$NFILES-2" | bc)
-    echo "$f,$NFILES,$NFNS,$NLOC"
-done | sort -t , -nk4
+    echo "$f,$NFILES,$NSTRUCTS,$NFNS,$NLOC"
+done | sort -t , -nk5
