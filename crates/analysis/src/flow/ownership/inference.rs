3d668b37c354d69eda12fd0784698e9a8fed4490
zhl <hanliangzhang.cm@gmail.com>
2023-10-12 13:53:39 +0100

    transfer constraint

--- crates/analysis/src/flow/ownership/inference.rs
+++ crates/analysis/src/flow/ownership/inference.rs
@@ -1,5 +1,6 @@
 use std::ops::Range;
 
+use either::Either::{Left, Right};
 use rustc_abi::FieldIdx;
 use rustc_index::IndexVec;
 use rustc_middle::{
@@ -7,13 +8,13 @@ use rustc_middle::{
         visit::Visitor, BasicBlock, BasicBlockData, BinOp, Body, Local, Location, NullOp, Operand,
         Place, Rvalue, Statement, Terminator, UnOp,
     },
-    ty::{Const, TyCtxt},
+    ty::{Const, Ty, TyCtxt},
 };
 use utils::data_structure::assoc::AssocExt;
 
 use super::{
-    access_path::Path,
-    constraint::{Database, OwnershipToken},
+    access_path::{AccessPaths, Path},
+    constraint::{Constraint, Database, OwnershipToken},
     Ctxt,
 };
 use crate::flow::{
@@ -42,12 +43,41 @@ type ExpandedBase = Update<OwnershipToken>;
 #[cfg(not(debug_assertions))]
 const _: () = assert!(std::mem::size_of::<ExpandedBase>() == 8);
 
-impl<'tcx, const K_LIMIT: usize, DB: Database> Analysis<'_, 'tcx, K_LIMIT, DB> {
-    // Cached?
-    fn base(&self, local: Local, ssa_idx: SSAIdx) -> OwnershipToken {
-        // monotonicity constraints assumed
-        self.tokens[local][ssa_idx]
+impl<T> Path<Update<T>> {
+    pub fn transpose(self) -> Update<Path<T>> {
+        let Path { base, projections } = self;
+        Update {
+            r#use: Path::new(base.r#use, projections),
+            def: Path::new(base.def, projections),
+        }
     }
+}
+
+fn ownership_tokens<'a, const K_LIMIT: usize>(
+    path: &Path<OwnershipToken>,
+    expected_depth: usize,
+    access_paths: &'a AccessPaths<K_LIMIT>,
+    ty: Ty,
+) -> impl Iterator<Item = OwnershipToken> + 'a {
+    if expected_depth == path.depth() {
+        Left(path.base..path.base + path.num_pointers_reachable())
+    } else {
+        assert!(expected_depth < path.depth());
+        let base = path.base;
+        Right(
+            access_paths
+                .patch_up(path.depth(), path.depth() - expected_depth, ty)
+                .map(move |offset| base + offset),
+        )
+    }
+}
+
+impl<'tcx, const K_LIMIT: usize, DB: Database> Analysis<'_, 'tcx, K_LIMIT, DB> {
+    // // Cached?
+    // fn base(&self, local: Local, ssa_idx: SSAIdx) -> OwnershipToken {
+    //     // monotonicity constraints assumed
+    //     self.tokens[local][ssa_idx]
+    // }
 
     /// If the path is a `Some`, then its size > 0
     fn path(&self, place: &Place<'tcx>, location: Location) -> Option<Path<Base>> {
@@ -55,8 +85,8 @@ impl<'tcx, const K_LIMIT: usize, DB: Database> Analysis<'_, 'tcx, K_LIMIT, DB> {
         let base = self.flow_chain.uses[location]
             .get_by_key(&place.local)
             .copied()?;
-        (path.num_pointers_reachable() > 0)
-            .then_some(Path::new((place.local, base), path.projections))
+        assert!(path.num_pointers_reachable() > 0);
+        Some(path.map_base(|()| (place.local, base)))
     }
 
     fn expand(&mut self, path: &Path<Base>) -> Path<ExpandedBase> {
@@ -93,11 +123,59 @@ impl<'tcx, const K_LIMIT: usize, DB: Database> Analysis<'_, 'tcx, K_LIMIT, DB> {
             .map(|ssa_idx| self.tokens[local][ssa_idx])
         })
     }
+
+    /// path1 = move path2
+    fn r#move(&mut self, path1: &Path<ExpandedBase>, path2: &Path<ExpandedBase>, ty: Ty<'tcx>) {
+        let min_depth = std::cmp::min(path1.depth(), path2.depth());
+        let path1 = path1.transpose();
+        let path2 = path2.transpose();
+
+        for x in ownership_tokens(&path1.r#use, min_depth, &self.ctxt.access_paths, ty) {
+            self.ctxt
+                .database
+                .add(Constraint::Assume { x, sign: false }, &mut ())
+        }
+        for (x, y) in ownership_tokens(&path1.def, min_depth, &self.ctxt.access_paths, ty).zip(
+            ownership_tokens(&path2.r#use, min_depth, &self.ctxt.access_paths, ty),
+        ) {
+            self.ctxt.database.add(Constraint::Equal { x, y }, &mut ())
+        }
+        for x in ownership_tokens(&path2.def, min_depth, &self.ctxt.access_paths, ty) {
+            self.ctxt
+                .database
+                .add(Constraint::Assume { x, sign: false }, &mut ())
+        }
+    }
+
+    fn transfer(&mut self, path1: &Path<ExpandedBase>, path2: &Path<ExpandedBase>, ty: Ty<'tcx>) {
+        let min_depth = std::cmp::min(path1.depth(), path2.depth());
+        let path1 = path1.transpose();
+        let path2 = path2.transpose();
+        for x in ownership_tokens(&path1.r#use, min_depth, &self.ctxt.access_paths, ty) {
+            self.ctxt
+                .database
+                .add(Constraint::Assume { x, sign: false }, &mut ())
+        }
+
+        for (x, y, z) in itertools::izip!(
+            ownership_tokens(&path1.def, min_depth, &self.ctxt.access_paths, ty),
+            ownership_tokens(&path2.def, min_depth, &self.ctxt.access_paths, ty),
+            ownership_tokens(&path2.r#use, min_depth, &self.ctxt.access_paths, ty)
+        ) {
+            self.ctxt
+                .database
+                .add(Constraint::Linear { x, y, z }, &mut ())
+        }
+    }
 }
 
 impl<'tcx, const K_LIMIT: usize, DB: Database> Visitor<'tcx> for Analysis<'_, 'tcx, K_LIMIT, DB> {
     fn visit_basic_block_data(&mut self, block: BasicBlock, data: &BasicBlockData<'tcx>) {
         for &(local, ref phi_node) in self.flow_chain.join_points[block].iter() {
+            // let def = phi_node.lhs;
+            // for r#use in phi_node.rhs.iter().copied() {
+
+            // }
             todo!(
                 "Equate the ownership variables at phi-node {local:?}: {:?} = {}",
                 phi_node.lhs,
@@ -113,12 +191,29 @@ impl<'tcx, const K_LIMIT: usize, DB: Database> Visitor<'tcx> for Analysis<'_, 't
     }
 
     fn visit_assign(&mut self, place: &Place<'tcx>, rvalue: &Rvalue<'tcx>, location: Location) {
-        let lhs = self.path(place, location);
+        // expand the path directly as it must be a definition
+        let Some(lhs) = self.path(place, location).map(|path| self.expand(&path)) else {
+            // if `lhs` is not a pointer, then `rhs` is unconstrained
+            return;
+        };
+        let ty = place.ty(self.body, self.tcx).ty;
         match rvalue {
             Rvalue::Use(operand) => match operand {
-                Operand::Copy(rhs) => {}
-                Operand::Move(_) => todo!(),
-                Operand::Constant(_) => todo!(),
+                Operand::Copy(rhs) => {
+                    let Some(rhs) = self.path(rhs, location).map(|path| self.expand(&path)) else {
+                        // if `rhs` is not a pointer, then `lhs` is unconstrained
+                        return;
+                    };
+                    self.transfer(&lhs, &rhs, ty)
+                }
+                Operand::Move(rhs) => {
+                    let Some(rhs) = self.path(rhs, location).map(|path| self.expand(&path)) else {
+                        // if `rhs` is not a pointer, then `lhs` is unconstrained
+                        return;
+                    };
+                    self.r#move(&lhs, &rhs, ty);
+                }
+                Operand::Constant(_) => return,
             },
             Rvalue::Repeat(_, _) => todo!(),
             Rvalue::Ref(_, _, _) => todo!(),
