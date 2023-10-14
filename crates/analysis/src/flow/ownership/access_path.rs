562dc8d5eb56acee4bbed91274a4025ec6a74fd1
zhl <hanliangzhang.cm@gmail.com>
2023-10-14 16:55:47 +0100

    add unsafe restrict

--- crates/analysis/src/flow/ownership/access_path.rs
+++ crates/analysis/src/flow/ownership/access_path.rs
@@ -117,6 +117,14 @@ impl<const K_LIMIT: usize> AccessPaths<K_LIMIT> {
         }
     }
 
+    pub fn restrict<const K_LIMIT1: usize>(self) -> AccessPaths<K_LIMIT1> {
+        assert!(K_LIMIT >= K_LIMIT1);
+        // TODO replace this with safe code
+        unsafe {
+            std::mem::transmute(self)
+        }
+    }
+
     pub fn post_order(&self) -> &[DefId] {
         &self.post_order.raw
     }
