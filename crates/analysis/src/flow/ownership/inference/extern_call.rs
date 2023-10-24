98885653cf8484150eb1f29db2407cce5711f084
zhl <hanliangzhang.cm@gmail.com>
2023-10-24 12:13:04 +0100

    handling realloc

--- crates/analysis/src/flow/ownership/inference/extern_call.rs
+++ crates/analysis/src/flow/ownership/inference/extern_call.rs
@@ -17,8 +17,8 @@ where
         location: Location,
     ) {
         match name.as_str() {
-            "malloc" => self.malloc(args, destination, location),
-            "calloc" => self.malloc(args, destination, location),
+            "malloc" | "calloc" => self.malloc(args, destination, location),
+            "realloc" => self.realloc(args, destination, location),
             "free" => self.free(args, destination, location),
             _ => tracing::debug!(
                 "ignoring extern call {destination:?} = {name}({})",
@@ -57,6 +57,59 @@ where
     fn free(&mut self, args: &Vec<Operand<'tcx>>, destination: &Place<'tcx>, location: Location) {
         assert!(self.path(destination, location).is_none());
         assert_eq!(args.len(), 1);
+        let arg = match args[0] {
+            Operand::Move(place) | Operand::Copy(place) => place,
+            _ => unreachable!(),
+        };
+        let Some(arg) = self.path(&arg, location) else {
+            assert_eq!(self.k_limit, 0);
+            return;
+        };
+        let arg = self.expand(&arg);
+        assert_eq!(arg.num_pointers_reachable(), 1);
+        self.ctxt.database.add(
+            Constraint::Assume {
+                x: arg.base.r#use,
+                sign: true,
+            },
+            &mut self.ctxt.storage,
+        );
+        self.ctxt.database.add(
+            Constraint::Assume {
+                x: arg.base.def,
+                sign: false,
+            },
+            &mut self.ctxt.storage,
+        );
+    }
+
+    fn realloc(
+        &mut self,
+        args: &Vec<Operand<'tcx>>,
+        destination: &Place<'tcx>,
+        location: Location,
+    ) {
+        let Some(dest) = self.path(destination, location) else {
+            assert_eq!(self.k_limit, 0);
+            return;
+        };
+        let dest = self.expand(&dest);
+        assert_eq!(dest.num_pointers_reachable(), 1);
+        self.ctxt.database.add(
+            Constraint::Assume {
+                x: dest.base.r#use,
+                sign: false,
+            },
+            &mut self.ctxt.storage,
+        );
+        self.ctxt.database.add(
+            Constraint::Assume {
+                x: dest.base.def,
+                sign: true,
+            },
+            &mut self.ctxt.storage,
+        );
+        assert_eq!(args.len(), 2);
         let arg = match args[0] {
             Operand::Move(place) | Operand::Copy(place) => place,
             _ => unreachable!(),
